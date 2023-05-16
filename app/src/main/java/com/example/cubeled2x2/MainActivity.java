2970ec4fe89846828a9123a4a18dfce3f79a5657
VikMakarik <vicka.makarova@mail.ru>
2023-05-16 15:04:29 +0300

    Доработка авторизации

--- app/src/main/java/com/example/cubeled2x2/MainActivity.java
+++ app/src/main/java/com/example/cubeled2x2/MainActivity.java
@@ -3,13 +3,10 @@ package com.example.cubeled2x2;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,18 +16,15 @@ public class MainActivity extends AppCompatActivity {
     private EditText password;
     private SharedPreferences preferences;
 
-    final Handler handler = new Handler();
-
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.authorization);
 
-        username = findViewById(R.id.edit_user);
-        password = findViewById(R.id.edit_password);
+        username = findViewById(R.id.edit_user_text);
+        password = findViewById(R.id.edit_password_text);
         Button login = findViewById(R.id.button_login);
-        TextView loginLocked = findViewById(R.id.login_locked);
         preferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
 
         // Получение данных пользователя из SharedPreferences
@@ -41,35 +35,33 @@ public class MainActivity extends AppCompatActivity {
         // Проверка и автоматическая авторизация, если данные пользователя сохранены
 
         if (!savedUsername.equals("") && !savedPassword.equals("")) {
-            login(savedUsername, savedPassword);
+//            login(savedUsername, savedPassword);
         }
 
-        // Если введенные логин ("user") и пароль ("pass") будут введены правильно,
+        // Если введенные логин ("makarik") и пароль ("10023") будут введены правильно,
         // показываем Toast сообщение об успешном входе:
 
         login.setOnClickListener(v -> {
             String enteredUsername = username.getText().toString();
             String enteredPassword = password.getText().toString();
-            if (enteredUsername.equals("user") && enteredPassword.equals("pass")) {
+            if (enteredUsername.equals("makarik") && enteredPassword.equals("10023")) {
                 preferences.edit().putString("username", enteredUsername).apply();
                 preferences.edit().putString("password", enteredPassword).apply();
+                Toast.makeText(this, "Вход выполнен успешно", Toast.LENGTH_SHORT).show();
             }
 
             else {
 
                 //Если данные неверные, показываем пользователю ошибку
 
-                Toast.makeText(this, "Неверные логин или пароль", Toast.LENGTH_SHORT).show();
-                loginLocked.setVisibility(View.VISIBLE);
-                loginLocked.setBackgroundColor(Color.RED);
-                loginLocked.setText("Вход заблокирован! Попробуйте еще раз!");
+                Toast.makeText(this, "Неверный логин или пароль", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    public void login(String savedUsername, String savedPassword) {
-        Intent intent = new Intent(this, MainScreen.class);
-        startActivity(intent);
-        finish();
-    }
+//    public void login(String savedUsername, String savedPassword) {
+//        Intent intent = new Intent(this, MainScreen.class);
+//        startActivity(intent);
+//        finish();
+//    }
 }
